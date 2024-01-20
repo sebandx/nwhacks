9c1e46abab670b13f1ac8b26df8933bc92526b00
juliansalvador727 <juliansalvador727@gmail.com>
2024-01-20 15:30:09 -0800

    added nav bar login and sign up

--- frontend-1/config/site.ts
+++ frontend-1/config/site.ts
@@ -3,12 +3,21 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   name: "Next.js",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Beautifully designed components built with Radix UI and bruh CSS.",
   mainNav: [
     {
       title: "Home",
       href: "/",
+    },  
+    {
+      title: "Login",
+      href:"/",
     },
+    {
+      title: "Sign Up",
+      href:"/", 
+    }
+    ,
   ],
   links: {
     twitter: "https://twitter.com/shadcn",
