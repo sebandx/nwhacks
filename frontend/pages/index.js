6a7a5e30f3d52e1c6dfccef94a6cf0ca26123946
michaely8 <151398889+michaely8@users.noreply.github.com>
2024-01-20 14:08:55 -0800

    update

---
+++ frontend/pages/index.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import ProTip from '../src/ProTip';
+import Link from '../src/Link';
+import Copyright from '../src/Copyright';
+import ResponsiveAppBar from '../src/ResponsiveAppBar';
+
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <ResponsiveAppBar />
+        <Typography variant="h4" component="h1" gutterBottom>
+          Material UI - Next.js example
+        </Typography>
+        <Link href="/about" color="secondary">
+          Go to the about page
+        </Link>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
