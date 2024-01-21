67ac39d80f656742cee42e236006deed0cd9df08
inkay_liu <110617789+sebandx@users.noreply.github.com>
2024-01-21 05:54:39 -0800

    changed to local

--- frontend/server.js
+++ frontend/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const http = require('http')
 const moment = require('moment');
 const socketio = require('socket.io');
-const PORT = process.env.PORT || 3000;
+const PORT = 4000;
 
 const app = express();
 const server = http.createServer(app);
