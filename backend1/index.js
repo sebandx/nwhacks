9ec46fc49b5123e639ad91c170d33947b1031308
inkay_liu <110617789+sebandx@users.noreply.github.com>
2024-01-21 03:06:30 -0800

    changed to be compatible with vercal

--- backend1/index.js
+++ backend1/index.js
@@ -1,31 +1,28 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 import userRoute from './routes/userRoute.js';
-const PORT = 5555;
-const mongoDBURL = "mongodb+srv://enkailiu1:IZFcooRhr1y3up3M@cluster0.5vaq3e0.mongodb.net/?retryWrites=true&w=majority";
 import cors from 'cors';
 import express from "express";
 
-const app = express();
-app.use(express.json());
+const mongoDBURL = "mongodb+srv://enkailiu1:IZFcooRhr1y3up3M@cluster0.5vaq3e0.mongodb.net/?retryWrites=true&w=majority";
 
-// Middleware for handling CORS POLICY
+const app = express();
+app.use(express.json()); 
+
 app.use(cors());
 
-
 app.get('/', (request, response) => {
-    console.log(request)
-    return response.status(234).send("sucess!")
+    console.log(request);
+    return response.status(200).send("success!");
 });
 
 app.use('/User/', userRoute);
-mongoose
-    .connect(mongoDBURL)
+
+mongoose.connect(mongoDBURL)
     .then(() => {
         console.log('App connected to database');
-        app.listen(PORT, () => {
-            console.log(`App is listening to port: ${PORT}`);
-        });
     })
     .catch((error) => {
-        console.log(error);
+        console.error('Database connection failed', error);
     });
+
+export default app;
