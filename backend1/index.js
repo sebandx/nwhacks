c95a58dbf0ab2e33c16ceb227b753c70295a33ec
inkay_liu <110617789+sebandx@users.noreply.github.com>
2024-01-21 03:53:31 -0800

    changed cors configuration

--- backend1/index.js
+++ backend1/index.js
@@ -8,7 +8,10 @@ const mongoDBURL = "mongodb+srv://enkailiu1:IZFcooRhr1y3up3M@cluster0.5vaq3e0.mo
 const app = express();
 app.use(express.json()); 
 
-app.use(cors());
+app.use(cors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}));
 
 app.get('/', (request, response) => {
     console.log(request);
